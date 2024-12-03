ab818be97529ba250b6c17c1968dca578082f8a7
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2024-12-02 20:15:45 -0600

    Create __init__.py

---
+++ datapunk/lib/shared/datapunk_shared/error/__init__.py
@@ -0,0 +1,11 @@
+# datapunk/lib/shared/datapunk_shared/error/__init__.py
+"""
+Error Module
+------------
+
+Provides error handling functionality including:
+- Error classification
+- Error reporting
+- Error logging
+"""
+
