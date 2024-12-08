66155801e66989fc909ea0aff245343c78fa1dd7
Distracted-E421 <138799590+Distracted-E421@users.noreply.github.com>
2024-12-08 16:05:33 -0600

    Update __init__.py

--- datapunk/containers/lake/src/storage/index/strategies/partitioning/distributed/__init__.py
+++ datapunk/containers/lake/src/storage/index/strategies/partitioning/distributed/__init__.py
@@ -1,10 +1,10 @@
-from .manager import DistributedPartitionManager
+from .distribution_manager import DistributedPartitionManager
 from .node import PartitionNode
 from .coordinator import PartitionCoordinator, ClusterState
 from .replication import ReplicationManager, ReplicationState
 from .health import HealthMonitor, HealthStatus
 from .network import NetworkManager, NetworkMessage, MessageTypes, NetworkMetrics
-from .consensus import ConsensusManager, ConsensusState
+from .distributed_consensus import ConsensusManager, ConsensusState
 from .recovery import RecoveryManager, BackupState
 
 __all__ = [
